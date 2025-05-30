5a39743a661fd953309a7b96728cfdf0ee291475
Asish Mehata <mehataasish@gmail.com>
2025-05-30 18:56:37 +0545

    Portfolio removed.

--- portfolio/main.js
+++
@@ -1,7 +0,0 @@
-let header = document.querySelector("header");
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-  header.classList.toggle("shadow", window.scrollY > 0);
-});

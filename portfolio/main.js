43f66b1549ee94d960778a70277afc35f0d64654
Asish Mehata <mehataasish@gmail.com>
2025-06-30 10:04:18 +0545

    updated

--- portfolio/main.js
+++ portfolio/main.js
@@ -5,3 +5,24 @@ let navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
 });
+
+
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault(); 
+
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId); 
+    
+
+    const headerOffset = header.offsetHeight; 
+    const elementPosition = targetElement.getBoundingClientRect().top; 
+    const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth' 
+    });
+  });
+});
